e8f0df1bf371987e28498f083e7c440070510a9e
keane putra setiawan <keane@CN23-R-0150.local>
2025-10-06 16:49:26 +0900

    feat: this is my first java program

---
+++ introduction/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main (String[] args) {
+        // This is my first JAVA program since universtiy
+        /*
+         * This 
+         * is a multiline comment
+         */
+        System.out.print("I like pizza!\n");
+        System.out.println("It's really good");
+        System.out.println("Buy me pizza!");
+    }
+
+}
